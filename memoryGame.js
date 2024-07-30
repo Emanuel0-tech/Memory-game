50fdbd9fe4d2be25ca4234a216ee03d7429611c5
Emanuel0-tech <MANELSILVA0830@GMAIL.COM>
2024-07-30 15:38:24 -0300

    Update memoryGame.js

--- memoryGame.js
+++ memoryGame.js
@@ -67,7 +67,7 @@ const createCard = (monster) => {
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
 
-    front.style.backgroundImage = `url("../Cards/${monster}.jpg")`;
+    front.style.backgroundImage = `url("Cards/${monster}.jpg")`;
 
     card.appendChild(front);
     card.appendChild(back);
